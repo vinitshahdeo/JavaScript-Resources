de6dca7331262ed6bd99461b81d6a497e8647123
Vinit Shahdeo <vinitshahdeo@gmail.com>
2021-12-11 15:59:17 +0530

    added code-snippet to flatten an array

---
+++ examples/flattenArray.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Flatten the given array
+ * Input: [1,2,[3,4,[5,6]],7,8,[9,10]] 
+ * Output: [1,2,3,4,5,6,7,8,9,10]
+ *
+ */
+const flattenArray = (arr) => {
+    let flattenedArray = [];
+    arr.forEach((item, index) => {
+        if(typeof item == 'object')
+           flattenedArray.push(...flattenArray(item));
+        else
+        flattenedArray.push(item);
+    })
+    return flattenedArray;
+}
+
+
+console.log(flattenArray([1,2,[3,4,[5,6,7]], 8]));
